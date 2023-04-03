87e84b064cb730e663b9eabd1b975f3e2807875d
Emory Fields <emory.c.fields@tamu.edu>
2023-04-03 17:39:57 -0500

    register page functioning

--- practice/register.php
+++ practice/register.php
@@ -1,93 +1,49 @@
 <?php 
 
 include "db_connection.php";
-
   if (isset($_POST['submit'])) {
-
     $first_name = $_POST['firstname'];
-
     $last_name = $_POST['lastname'];
-
     $email = $_POST['email'];
-
     $password = $_POST['password'];
-
     $gender = $_POST['gender'];
-
     $sql = "INSERT INTO `users`(`firstname`, `lastname`, `email`, `password`, `gender`) 
-
            VALUES ('$first_name','$last_name','$email','$password','$gender')";
-
     $result = $conn->query($sql);
-
     if ($result == TRUE) {
-
       echo "New record created successfully.";
-
     }else{
-
       echo "Error:". $sql . "<br>". $conn->error;
-
     }
-
     $conn->close();
-
   } 
-
 ?> 
 
 <!DOCTYPE html>
-
 <html>
-
 <body>
-
 <h2>Registration Form</h2> 
-
 <form action="" method="POST">
-
   <fieldset>
-
     <legend>Information:</legend>
-
     First name:<br>
-
     <input type="text" name="firstname">
-
     <br>
-
     Last name:<br>
-
     <input type="text" name="lastname">
-
     <br>
-
     Email:<br>
-
     <input type="email" name="email">
-
     <br>
-
     Password:<br>
-
     <input type="password" name="password">
-
     <br>
-
     Gender:<br>
-
     <input type="radio" name="gender" value="Male">Male
-
     <input type="radio" name="gender" value="Female">Female
-
     <br><br>
-
     <input type="submit" name="submit" value="submit">
-
   </fieldset>
-
 </form> 
-
 </body>
-
 </html>
