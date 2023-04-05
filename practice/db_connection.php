4a430f753f039990103d9b5a1e9fa9b461fc97b5
Allison Edwards <allisonedwards12@yahoo.com>
2023-04-05 16:31:47 -0500

    edits to make system work on my machine

--- practice/db_connection.php
+++ practice/db_connection.php
@@ -1,18 +1,10 @@
 <?php
-function OpenCon()
- {
- $dbhost = "localhost";
- $dbuser = "root";
- $dbpass = "t(Ij1DXiSPW8w*SY";
- $db = "practice";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
- 
- return $conn;
- }
- 
-function CloseCon($conn)
- {
- $conn -> close();
- }
-   
+    $servername = "localhost";
+    $username = "root";
+    $password = "t(Ij1DXiSPW8w*SY";
+    $dbname = "practice"; 
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if($conn->connect_error) {
+        die("Connection Failed" . $conn->connect_error);
+    }
 ?>
