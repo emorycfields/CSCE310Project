6067c00e0a04b3e47a614b24c92b09902e9962e3
Allison Edwards <allisonedwards12@yahoo.com>
2023-04-05 17:28:15 -0500

    edited to work for new database

--- db_connection.php
+++ db_connection.php
@@ -4,7 +4,7 @@ function OpenCon()
  $dbhost = "localhost";
  $dbuser = "root";
  $dbpass = "t(Ij1DXiSPW8w*SY";
- $db = "practice";
+ $db = "schedule";
  $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
  
  return $conn;
