8b73d936831194f11fd9ea7b21600c1db8c0a36b
Emory Fields <emory.c.fields@tamu.edu>
2023-04-16 21:39:48 -0500

    worked on home, projects, newproject, projectdetails

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> 
+    <h1 align="left"> Home </h1>  
+</head>
+</html>
+
+
+<div class="d-flex flex-grow-1 justify-content-center align-items-center">
+  <div class="container py-4 ">
+      <div class="card-deck-wrapper">
+          <div class="card-deck ">
+              <div class="card  bg-light" style = "width: 30rem; height: 10rem ">
+                  <a class="card-block stretched-link text-decoration-none" href = "projects.php" style="color:black">
+                      <h3 align="center">Projects</h3>
+                  </a>
+              </div>
+              <div class="card bg-light" style = "width: 30rem; height: 10rem ">
+                  <a class="card-block stretched-link text-decoration-none" href="schedule.php" style="color:black">
+                      <h3 align="center">Schedule</h3>
+                  </a>
+              </div>
+              <div class="card bg-light" style = "width: 30rem; height: 10rem ">
+                  <a class="card-block stretched-link text-decoration-none" href = "register.php" style="color:black">
+                      <h3 align="center" >Add User</h3>
+                  </a>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
